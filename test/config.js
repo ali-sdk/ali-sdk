0270544aca76802e8ac7fab150ee12f1b1699fb2
fengmk2 <fengmk2@gmail.com>
2015-02-24 16:57:24 +0800

    try to fix secure on travis.yml

--- test/config.js
+++ test/config.js
@@ -6,6 +6,6 @@ module.exports = {
     port: env.ALI_SDK_RDS_PORT || 3306,
     user: env.ALI_SDK_RDS_USER || 'root',
     password: env.ALI_SDK_RDS_PASSWORD || '',
-    database: 'ali-sdk-test-user',
+    database: env.ALI_SDK_RDS_DATABASE || 'ali-sdk-test-db',
   }
 };
